952f558c4d578208efdacb8b247b2c565136e433
Chalmers <ajcyrw@umsystem.edu>
2026-09-24 12:36:33 -0500

    Added to files

--- metrics.py
+++ metrics.py
@@ -1,9 +1,22 @@
-def mean():
-    return
+def mean(data):
+    return sum(data) / len(data) if data else 0 
 
 def mode():
     return
 
-def zscore():
+def median():
     return
+
+def variance():
+    sum_of_squares = sum((x - mean(data)) ** 2 for x in data)
+
+    if len(data) > 1:
+        return sum_of_squares / (len(data) - 1) 
+    return 
+
+def stddev():
+    return variance() ** 0.5
+
+def zscore():
+    return (x - mean(data)) / stddev() if stddev() else 0
 
--- visualization.py
+++ visualization.py
@@ -8,3 +8,19 @@ def histogram(data, bins=10, title='Histogram', xlabel='Value', ylabel='Frequenc
     plt.ylabel(ylabel)
     plt.show()
 
+
+def scatter_plot(x, y, title='Scatter Plot', xlabel='X-axis', ylabel='Y-axis'):
+    plt.scatter(x, y)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+def box_plot(data, title='Box Plot', xlabel='Value'):
+    plt.boxplot(data)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.show()  
+    
+
+    
